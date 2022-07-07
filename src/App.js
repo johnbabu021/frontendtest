3d9c93e1096416a992134c6b0f2bf14b53089df0
johnbabu021 <johnbabu021@gmail.com>
2022-07-07 09:13:17 +0530

    init:initializing the project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    //createing a login page
+    //no routes are described
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
